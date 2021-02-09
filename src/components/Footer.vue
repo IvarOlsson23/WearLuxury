3fb578bf6a6093d15c65996c9be8e48becada8c8
maryam008@hotmail.se <>
2021-02-09 19:31:09 +0100

    added footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,27 @@
 <template>
-<div>
-     <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-toggle target="nav-collapse"/>
-    <b-navbar-nav class="ml-auto footernavbar">
-      <p>Tel: 032843417</p>
-      <p>E-mail: WLShop@hotmail.com</p>
-        <b-icon icon="facebook" font-scale="1"/>
-          <b-icon icon="instagram" font-scale="1"/>
-           <b-icon icon="twitter" font-scale="1"/>
-</b-navbar>
-</div>
+  <div>
+    <nav class="navbar navbar-light bg-light">
+      <div class="footerNavbar">
+        <p class="footerNavbar">Contact info:</p>
+        <p class="footerNavbar">Tel: 032843417</p>
+
+        <p class="footerNavbar">e-mail: WLShop@hotmail.com</p>
+
+        <b-icon style="margin-left:1.5em" icon="facebook" font-scale="1" />
+        <b-icon style="margin-left:1.5em" icon="instagram" font-scale="1" />
+        <b-icon style="margin-left:1.5em" icon="twitter" font-scale="1" />
+      </div>
+    </nav>
+  </div>
 </template>
+
+<style scoped>
+  .footerNavbar {
+    color: rgb(229, 230, 231);
+    text-align: center;
+    background-color: rgb(53, 53, 53);
+    width: 1350px;
+    margin-bottom: 10px;
+    margin-top: 10px;
+  }
+</style>
