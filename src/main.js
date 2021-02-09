676e3aaa33d55d0f7bc963c4ee80260739db74ef
Ivar Olsson <ivar.olsson2323@gmail.com>
2021-02-09 13:52:50 +0100

    Start-Project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import './main.scss'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
