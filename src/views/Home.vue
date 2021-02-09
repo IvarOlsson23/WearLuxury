49257a1c924e8bf6829111bbe0ff67c66422b81e
Sofia Premuzaj <sofia.premuzaj@gmail.com>
2021-02-09 15:54:14 +0100

    made some changes on productCard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <productCard style="width:300px" />
+    
+
   </div>
 </template>
 
 <script>
   // @ is an alias to /src
   import HelloWorld from '@/components/HelloWorld.vue'
+import ProductCard from '@/components/productCard.vue'
 
   export default {
     name: 'Home',
     components: {
-      HelloWorld
+      HelloWorld,
+        ProductCard
     }
   }
 </script>
