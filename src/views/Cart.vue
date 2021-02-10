050e26fdc1561f2097d9c9252997abf4e2b0dedb
Thomas <tumppu91@hotmail.com>
2021-02-10 14:35:02 +0100

    added picture to card

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -5,6 +5,8 @@
         <b-card title="Cart (5) products" class="h-100">
           <b-card
             img-src="https://img01.ztat.net/article/spp-media-p1/5e0b5adffa1338618afdae5031987c3e/0d68e7d30bf0477f93d4d0a7272ae7cc.jpg?imwidth=1800"
+            img-left
+            img-width="100px"
           >
             <b-card-text>Products</b-card-text>
           </b-card>
