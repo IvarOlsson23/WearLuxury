d781a6a4a3c1dc76ff0adc854d99c1f2d6841130
Thomas <tumppu91@hotmail.com>
2021-02-10 14:31:03 +0100

    working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,13 +37,9 @@
   import ProductCard from '@/components/productCard.vue'
   export default {
     name: 'Home',
-<<<<<<< HEAD
     components: {
       ProductCard
     },
-=======
-    components: {},
->>>>>>> b8b53a67c2150cb2c05aa5e7fd257bb54c7d4011
     data() {
       return {
         productOverlay: true
@@ -68,7 +64,6 @@
     left: 0px;
     margin: 0%;
   }
-<<<<<<< HEAD
 
   #product-wrapper1 {
     display: flex;
@@ -99,75 +94,6 @@
   #p2 {
     margin-top: 40vh;
     margin-right: 10%;
-=======
-  #product-pic1 {
-    background-color: #777978;
-    margin-left: 50%;
-    margin-top: 8vh;
-    width: 540px;
-    height: 700px;
-    left: 982px;
-    top: 780px;
-    box-shadow: -8px 8px 21px rgba(0, 0, 0, 0.25);
-  }
-  #overlay1 {
-    background-color: #353535;
-    position: absolute;
-    width: 329px;
-    height: 324px;
-    left: 818px;
-    top: 1334px;
-  }
-  #text1 {
-    position: absolute;
-    width: 410px;
-    height: 199px;
-    left: 268px;
-    top: 1046px;
-  }
-  #product-pic2 {
-    background-color: #777978;
-    position: absolute;
-    width: 606px;
-    height: 700px;
-    left: 358px;
-    top: 1771px;
-    box-shadow: -8px 8px 21px rgba(0, 0, 0, 0.25);
-  }
-  #overlay2 {
-    background-color: #353535;
-    position: absolute;
-    width: 329px;
-    height: 324px;
-    left: 800px;
-    top: 2300px;
-  }
-  #text2 {
-    position: absolute;
-    width: 410px;
-    height: 199px;
-    left: 1152px;
-    top: 2019px;
-  }
-  #footer {
-    position: absolute;
-    width: 1920px;
-    height: 376px;
-    left: 0px;
-    top: 2800px;
-    background-color: #333333;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .nav-buttons {
-    color: rgb(221, 221, 221);
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    padding: 10px;
-    justify-content: center;
->>>>>>> b8b53a67c2150cb2c05aa5e7fd257bb54c7d4011
   }
   h3 {
     margin: 40px 0 0;
