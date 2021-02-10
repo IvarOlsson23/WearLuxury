4ee3a33e73330b148733e2e5dbd88d4c9f64cbc7
maryam008@hotmail.se <>
2021-02-11 00:43:19 +0100

    uppdated products.vue

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,41 +1,62 @@
 <template>
-<div class="produts">
+  <div class="produts">
     <b-container class="bv-example-row bv-example-row-flex-cols">
-        <b-row class="menue" align-v="end bg-dark">
-    <b-col>Menue</b-col>
-  </b-row><b-row class="login" align-v="center bg-dark">
-    <b-col offset="1">Login</b-col>
-  </b-row>
-<b-dropdown id="dropdown-1" text="Sort by" class="m-md-2 bg-dark" align-v="left">
-    <b-dropdown-item>Color</b-dropdown-item>
-    <b-dropdown-item>Fabric</b-dropdown-item>
-    <b-dropdown-item>Pattern</b-dropdown-item>
-    <b-dropdown-divider/>
-  </b-dropdown>
-  
-  <b-row align-v="left">
-    <b-col>One of three columns</b-col>
-    <b-col>One of three columns</b-col>
-    <b-col>One of three columns</b-col>
-  </b-row>
+      <b-row>
+        <b-col cols="3" />
+        <b-col cols="6" class="textcenter">
+          <img src="logo.png" width="150px" />
+        </b-col>
+        <b-col cols="3">
+          <div class="float-right">
+            <b-dropdown
+              id="dropdown-1"
+              text="Sort by"
+              class="m-md-2 bg-dark"
+              align-v="left"
+            >
+              <b-dropdown-item>Color</b-dropdown-item>
+              <b-dropdown-item>Fabric</b-dropdown-item>
+              <b-dropdown-item>Pattern</b-dropdown-item>
+              <b-dropdown-divider />
+            </b-dropdown>
+            <b-dropdown
+              id="dropdown-1"
+              text="Products"
+              class="m-md-2 bg-dark"
+              align-v="left"
+            >
+              <b-dropdown-item>Women clothes</b-dropdown-item>
+              <b-dropdown-item>Men clothes</b-dropdown-item>
+              <b-dropdown-item>Accessories</b-dropdown-item>
+              <b-dropdown-divider />
+            </b-dropdown>
+          </div>
+        </b-col>
+      </b-row>
 
+      <div class="dressblack">
+        <b-col><img src="blackdress.jpg" width="170px"/></b-col>
 
-  <b-row align-v="baseline">
-    <b-col style="font-size: 0.75rem;">One of three columns</b-col>
-    <b-col>One of three columns</b-col>
-    <b-col style="font-size: 1.25rem;">One of three columns</b-col>
-  </b-row>
-
- 
-</b-container>
-</div>
+        <b-row>999kr</b-row>
+      </div>
+      <b-row>
+        <div class="dressbox">
+          <b-col><img src="blackcoat.jpg" width="145px"/></b-col>
+        </div>
+        <div class="watch bg-light">
+          <b-col><img src="watch.jpg" width="170px"/></b-col>
+        </div>
+        <div class="purse">
+          <b-col><img src="purse.jpg" width="170px"/></b-col>
+        </div>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <style>
   .login {
     color: white;
-    width: 200px;
-    padding: 10px;
   }
   .menue {
     align-content: center;
@@ -43,10 +64,35 @@
     width: 150px;
     float: right;
     padding: 20px;
-}
-.dropdown-1{
+  }
+  .dropdown-1 {
     float: left;
- 
-}
-
+  }
+  .dressblack {
+    width: 300px;
+    height: 200px;
+    border: solid 1px rgba(185, 184, 184, 0.747);
+    font-style: bold;
+  }
+  .dressbox {
+    border: solid 1px rgba(185, 184, 184, 0.747);
+    width: 300px;
+    height: 250px;
+    margin-top: 50px;
+    margin-left: 15px;
+  }
+  .watch {
+    border: solid 1px rgba(185, 184, 184, 0.747);
+    width: 300px;
+    height: 250px;
+    margin-top: 50px;
+    margin-left: 15px;
+  }
+  .purse {
+    border: solid 1px rgba(185, 184, 184, 0.747);
+    width: 250px;
+    height: 250px;
+    margin-top: 50px;
+    margin-left: 15px;
+  }
 </style>
