4757331afd378fe2f716141e4e70f4b1acd1a4d1
Thomas <tumppu91@hotmail.com>
2021-02-11 14:42:12 +0100

    added cart state and so it works in cart page

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,32 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    cart: {
+      '5895af63-e246-42bc-b629-bf607f004da7': {
+        brand: 'Dior',
+        name: 'Jacket',
+        price: '2900',
+        items: 2,
+        color: 'black',
+        size: 'xl'
+      },
+
+      '94a1304a-8a25-459c-aa9a-e439e25261a9': {
+        brand: 'Hugo boss',
+        name: 'Dress',
+        price: '2499',
+        items: 5,
+        color: 'red',
+        size: 'sm'
+      }
+    }
+  },
+  mutations: {
+    setCartItems(name) {
+      this.state.cart(name)
+    }
+  },
   actions: {},
   modules: {}
 })
