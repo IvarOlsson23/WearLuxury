08fe172b9caf7591342404c53df2824c7187d753
araf01 <nevzatarif@gmail.com>
2021-02-16 13:50:54 +0100

    ny nav

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,15 @@
     }
   }
 </script>
-<style scoope lang="scss">
+<style scoped lang="scss">
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+  
+    
+  
   }
 </style>
