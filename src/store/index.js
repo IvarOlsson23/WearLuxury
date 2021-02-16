25645d47954d8dd570d2a81b8ca15c903a9f0dd0
Dorsa Mehrzad <dorsa_mehrzad@yahoo.com>
2021-02-16 14:49:28 +0100

    updated order

--- src/store/index.js
+++ src/store/index.js
@@ -138,8 +138,48 @@ export default new Vuex.Store({
         ]
       }
     },
+    newarrivalproducts: [
+      {
+        title: 'Black lace dress',
+        img: 'blackdress.jpg',
+        price: '1999kr',
+        cathegory: 'women'
+      },
+
+      {
+        title: 'Black coat',
+        img: 'blackcoat.jpg',
+        price: '2809kr',
+        cathegory: 'men'
+      },
+      {
+        title: 'Golden watch',
+        img: 'watch.jpg',
+        price: '2799kr',
+        cathegory: 'accessories'
+      },
+      {
+        title: 'Gold earings',
+        img: 'earings.jpg',
+        price: '800kr',
+        cathegory: 'accessories'
+      },
+      {
+        title: 'Pear black purse',
+        img: 'purse.jpg',
+        price: '2999kr',
+        cathegory: 'bags'
+      },
+      {
+        title: 'Leather shoes',
+        img: 'shoes.jpg',
+        price: '3999kr',
+        cathegory: 'shoes'
+      }
+    ],
+
     orders: [],
-    cart: [1, 2, 3]
+    cart: []
   },
   mutations: {
     setOrder(state, order) {
@@ -157,9 +197,6 @@ export default new Vuex.Store({
   getters: {
     newarrivalproducts(state) {
       return state.newarrivalproducts
-    },
-    countItems(state) {
-      return state.cart.length
     }
   }
 })
