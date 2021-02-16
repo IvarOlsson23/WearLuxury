f690ef0f5fa77745fb4656b424cd9eb64012870b
araf01 <nevzatarif@gmail.com>
2021-02-16 15:03:30 +0100

    nav med varukorg

--- src/store/index.js
+++ src/store/index.js
@@ -169,46 +169,6 @@ export default new Vuex.Store({
         ]
       }
     },
-    newarrivalproducts: [
-      {
-        title: 'Black lace dress',
-        img: 'blackdress.jpg',
-        price: '1999kr',
-        cathegory: 'women'
-      },
-
-      {
-        title: 'Black coat',
-        img: 'blackcoat.jpg',
-        price: '2809kr',
-        cathegory: 'men'
-      },
-      {
-        title: 'Golden watch',
-        img: 'watch.jpg',
-        price: '2799kr',
-        cathegory: 'accessories'
-      },
-      {
-        title: 'Gold earings',
-        img: 'earings.jpg',
-        price: '800kr',
-        cathegory: 'accessories'
-      },
-      {
-        title: 'Pear black purse',
-        img: 'purse.jpg',
-        price: '2999kr',
-        cathegory: 'bags'
-      },
-      {
-        title: 'Leather shoes',
-        img: 'shoes.jpg',
-        price: '3999kr',
-        cathegory: 'shoes'
-      }
-    ],
-
     orders: [],
     cart: []
   },
@@ -228,6 +188,9 @@ export default new Vuex.Store({
   getters: {
     newarrivalproducts(state) {
       return state.newarrivalproducts
+    },
+    countItems(state) {
+      return state.cart.length
     }
   }
 })
