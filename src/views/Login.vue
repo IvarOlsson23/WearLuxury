d0b5cb205a859a409edf97e4fcfe1134a8054c39
Dorsa Mehrzad <dorsa_mehrzad@yahoo.com>
2021-02-23 14:36:41 +0100

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,6 +3,43 @@
     <b-row class="vh-100">
       <b-col>
         <h1>Login</h1>
+        <div>
+  <b-form @submit.stop.prevent style="max-width: 540px; padding:10px; margin: 15px">
+    <label for="text-email">E-mail</label>
+    <b-form-input type="email" id="text-email" aria-describedby="email-help-block"/>
+   
+   </b-form>
+
+   <b-form @submit.stop.prevent style="max-width: 540px; padding: 10px; margin: 15px">
+    <label for="text-password">Password</label>
+    <b-form-input type="password" id="text-password" aria-describedby="password-help-block"/>
+    
+   </b-form>
+</div>
+
+<div>
+    <b-form-checkbox style="max-width: 540px; padding: 10px; margin: 15px"
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      Remember me
+    </b-form-checkbox>
+
+   <div>
+  <b-link style="max-width: 540px; padding: 15px; margin: 15px" href="#">Forgot password? </b-link>
+</div>
+  </div>
+
+  <div>
+  <b-button block variant="secondary" style="max-width: 700px; padding: 15px; margin: 15px" >Login</b-button>
+</div>
+
+<div>
+  <b-button block variant="secondary" style="max-width: 700px; padding: 15px; margin: 15px" >Create Account</b-button>
+</div>
       </b-col>
     </b-row>
   </div>
