9a7673ab33df78e407594e3cffb6f29a55dcc393
Dorsa Mehrzad <dorsa_mehrzad@yahoo.com>
2021-02-24 16:27:25 +0100

    forgotpassword

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -41,12 +41,39 @@
             Remember me
           </b-form-checkbox>
 
-          <div style="max-width: 540px; padding: 10px; margin-left: 200px">
-            <b-link
-              
-              href="#" 
-              >Forgot password?
-            </b-link>
+          <div>
+            <div class="mt-3" v-b-modal.modal-prevent-closing style="max-width: 540px; padding: 10px; margin-left: 100px;">
+              Forgot password?
+              <div v-if="submittedEmail.length === 0" />
+              <ul v-else class="mb-0 pl-3">
+                <li v-for="email in submittedEmail" :key="email">{{ email }}</li>
+              </ul>
+            </div>
+
+            <b-modal
+              id="modal-prevent-closing"
+              ref="modal"
+              title="Forgotten password?"
+              @show="resetModal"
+              @hidden="resetModal"
+              @ok="handleOk"
+            >
+              <form ref="form" @submit.stop.prevent="handleSubmit">
+                <b-form-group
+                  label="Enter your E-mail"
+                  label-for="email-input"
+                  invalid-feedback="E-mail is correct"
+                  :state="emailState"
+                >
+                  <b-form-input
+                    id="email-input"
+                    v-model="email"
+                    :state="emailState"
+                    required
+                  />
+                </b-form-group>
+              </form>
+            </b-modal>
           </div>
         </div>
 
@@ -75,7 +102,40 @@
 <script>
   export default {
     data() {
-      return {}
+      return {
+        email: '',
+        emailState: null,
+        submittedEmail: ''
+      }
+    },
+    methods: {
+      checkFormValidity() {
+        const valid = this.$refs.form.checkValidity()
+        this.emailState = valid
+        return valid
+      },
+      resetModal() {
+        this.email = ''
+        this.emailState = null
+      },
+      handleOk(bvModalEvt) {
+        // Closing
+        bvModalEvt.preventDefault()
+        // Submit handle
+        this.handleSubmit()
+      },
+      handleSubmit() {
+        // Exit when the form isn't valid
+        if (!this.checkFormValidity()) {
+          return
+        }
+        // Push the email to submitted emails
+        this.submittedEmail.push(this.email)
+        // Hide the modal manually
+        this.$nextTick(() => {
+          this.$bvModal.hide('modal-prevent-closing')
+        })
+      }
     }
   }
 </script>
@@ -96,6 +156,5 @@
     margin-left: 20px;
     padding-top: 20px;
     margin-bottom: 30px;
-    
   }
 </style>
