3b68cdd6111703a529cc06c8138bcb0cf2213971
maryam008@hotmail.se <>
2021-02-26 13:04:51 +0100

    uppdated about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,15 +7,34 @@
             <div class="flexbox" style="background-image: url('/social.jpeg');">
               Social responsibility
             </div>
-            <p>dfs</p>
+            <p class="text">
+              <strong>Wear Luxury</strong> strives to have a positive
+              <br />impact not only within our stores and corporate family, but
+              also upon the<br />
+              environment and on the hundreds of vendor manufacturing
+              facilities<br />
+              throughout the world and their employees.
+            </p>
           </b-col>
           <b-col cols="4">
             <div class="flexbox" style="background-image: url('/policy.jpg');">
-              Our policy
+              Privacy policy
             </div>
+            <p class="text">
+              <strong>our main goals</strong> are to gain and maintain your
+              trust,<br />
+              including by addressing any questions or concerns you might have
+              about the privacy and <br />safety of yourpersonal information
+              when it is in our hands. In this regard,<br />
+              we have partnered with One Trust, a leading technology platform,
+              to allow us to better communicate with you and to respond to your
+              requests more efficiently.
+            </p>
           </b-col>
           <b-col cols="4">
-            <div class="flexbox" style="background-image: url('/blog.jpg');" />
+            <div class="flexbox" style="background-image: url('/career.jpg');">
+              Careers
+            </div>
           </b-col>
         </b-row>
       </b-col>
@@ -34,5 +53,9 @@
     align-items: center;
     height: 200px;
     background-size: cover;
+  }
+  .text {
+    border: solid 1px rgb(172, 172, 172);
+    font-size: 15px;
   }
 </style>
