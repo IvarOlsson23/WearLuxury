c1a057b3cfc169db51a6104b779ded6e60695f8c
Thomas Laukkanen <tumppu91@hotmail.com>
2021-03-01 15:53:12 +0100

    Footer is now completly responsive and looks good

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,96 +2,115 @@
   <div class="footer-container overflow-hidden">
     <nav class="navbar navbar-light bg-primary p-0">
       <div class="footerNavbar mt-5">
-        <b-row>
-          <b-col cols="8" offset="2"
-            ><b-row>
-              <b-col class="text-left col-sm-12 col-md-3">
-                <strong>Help</strong>
-                <ul class="footerlist">
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >Order status</router-link
-                    >
-                  </li>
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >My account</router-link
-                    >
-                  </li>
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >Contact us</router-link
-                    >
-                  </li>
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >Shipping info</router-link
-                    >
-                  </li>
-                </ul>
-              </b-col>
-              <b-col class="text-left col-sm-12 col-md-3">
-                <strong>Company info</strong>
-                <ul class="footerlist">
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >About us</router-link
-                    >
-                  </li>
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >Social responsibility</router-link
-                    >
-                  </li>
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >Our policy</router-link
-                    >
-                  </li>
-                </ul>
-              </b-col>
-              <b-col class="text-left col-sm-12 col-md-3">
-                <strong>Membership</strong>
-                <ul class="footerlist">
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >Get 20% off by <br />
-                      becoming a member</router-link
-                    >
-                  </li>
-                </ul>
-              </b-col>
-              <b-col class="text-left col-sm-12 col-md-3">
-                <strong>Quick links</strong>
-                <ul class="footerlist">
-                  <li>
-                    <router-link to="/Products" class="footerlink"
-                      >Special offers</router-link
-                    >
-                  </li>
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >Careers</router-link
-                    >
-                  </li>
-                  <li>
-                    <router-link to="/About" class="footerlink"
-                      >Gift cards</router-link
-                    >
-                  </li>
-                </ul>
-              </b-col>
-            </b-row>
+        <b-row cols="2" class="borders d-flex justify-content-center ">
+          <b-col
+            sm="6"
+            md="3"
+            class="text-left borders d-flex justify-content-center"
+          >
+            <ul class="footerlist ">
+              <li>
+                <h5>
+                  <strong>Help</strong>
+                </h5>
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >Order status</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >My account</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >Contact us</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >Shipping info</router-link
+                >
+              </li>
+            </ul>
+          </b-col>
+          <b-col sm="6" md="3" class="d-flex justify-content-center">
+            <ul class="footerlist ">
+              <li>
+                <h5>
+                  <strong>
+                    Company info
+                  </strong>
+                </h5>
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >About us</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >Social responsibility</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >Our policy</router-link
+                >
+              </li>
+            </ul>
+          </b-col>
+          <b-col
+            sm="6"
+            md="3"
+            class="text-left borders d-flex justify-content-center "
+          >
+            <ul class="footerlist ">
+              <li>
+                <h5>
+                  <strong>Membership</strong>
+                </h5>
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >Get 20% off by <br />
+                  becoming a member</router-link
+                >
+              </li>
+            </ul>
+          </b-col>
+          <b-col sm="6" md="3" class="text-left d-flex justify-content-center">
+            <ul class="footerlist ">
+              <li>
+                <h5>
+                  <strong>Quick links</strong>
+                </h5>
+              </li>
+              <li>
+                <router-link to="/Products" class="footerlink"
+                  >Special offers</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >Careers</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/About" class="footerlink"
+                  >Gift cards</router-link
+                >
+              </li>
+            </ul>
           </b-col>
         </b-row>
-        <!-- <p class="footerNavbar">Tel: 032843417</p>
-
-        <p class="footerNavbar">E-mail: WLShop@hotmail.com</p> -->
 
         <div class="icons my-4">
-          <b-icon class="mx-4" icon="facebook" font-scale="1.5" />
-          <b-icon class="mx-4" icon="instagram" font-scale="1.5" />
-          <b-icon class="mx-4" icon="twitter" font-scale="1.5" />
+          <b-icon class="mx-4 cursor" icon="facebook" font-scale="1.5" />
+          <b-icon class="mx-4 cursor" icon="instagram" font-scale="1.5" />
+          <b-icon class="mx-4 cursor" icon="twitter" font-scale="1.5" />
         </div>
       </div>
     </nav>
@@ -101,18 +120,21 @@
 <style scoped>
   .footerlink {
     color: white;
-    font-size: 11px;
+    /* font-size: 11px; */
     font-weight: lighter;
+    line-height: 1.6;
   }
   .footerlist {
     list-style: none;
     text-align: left;
     padding: 0px;
+    /* border: 2px solid red; */
+    /* text-align: center; */
+    max-width: 120px;
   }
 
   .footerNavbar {
     color: rgb(231, 231, 229);
-
     width: 100%;
   }
 
@@ -121,9 +143,16 @@
     bottom: 0;
     width: 100%;
   }
-  /* @media only screen and (max-width: 800px) {
-    .footerNavbar {
-      width: 100%;
+
+  .borders {
+    /* border: 2px solid green; */
+  }
+  .cursor:hover {
+    cursor: pointer;
+  }
+  @media (min-width: 768px) {
+    .footerlist {
+      max-width: 170px;
     }
-  } */
+  }
 </style>
